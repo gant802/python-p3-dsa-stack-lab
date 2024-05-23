5da08ebb1dee9bfd2ea451e1b6c111ca1e111462
Grant Cummings <grantcummings4429@gmail.com>
2024-05-23 11:32:54 -0400

    finished lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,46 @@
+import ipdb
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
+
+    def __repr__(self):
+        return f"Stack({self.items})"
 
     def isEmpty(self):
-        pass
+        if not self.items:
+            return True
+        else : 
+            return False
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            return self.items.append(item)
 
     def pop(self):
-        pass
-
+        if not self.isEmpty():
+            return self.items.pop()
+        
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if not self.isEmpty():
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+        if self.items[-1] == target:
+            return 0
+        if target in self.items:
+            return (len(self.items) - self.items.index(target)) - 1
+        else : 
+            return -1
+            
+stk = Stack([5,6,7,8,9,10])
+
